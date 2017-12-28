8568969d44370a5c537dc1194415e89caa0a07fe
Sebastian Sobotka <sebastian.sobotka@gmail.com>
2017-12-28 17:33:35 +0100

    test commit

--- favFruits/ViewController.swift
+++ favFruits/ViewController.swift
@@ -82,7 +82,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("start app with git")
+        //print("start app with git")
        
     }
 
